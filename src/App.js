a5eaa7aa7164797bb60366979ddbcf206f2181ba
Diego Azevedo <dapptn@hotmail.com>
2020-03-26 16:58:26 +1100

    global style

--- src/App.js
+++ src/App.js
@@ -1,64 +1,15 @@
 import React from 'react';
-import { createGlobalStyle } from 'styled-components';
-import Reset from 'styled-reset';
 import { BrowserRouter } from 'react-router-dom';
 
 import Routes from './routes';
-// import history from './services/history';
-
-const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap');
-  ${Reset}
-  * {
-    margin: 0;
-    padding: 0;
-    outline: none;
-    box-sizing: border-box;
-  }
-
-  *:focus {
-    outline: 0;
-  }
-
-  html, body, #root {
-    margin: 0;
-    height: 100%;
-  }
-
-  body {
-    -webkit-font-smoothing: antialised;
-    /* overflow: hidden; */
-    /* height: 100%; */
-  }
-
-  body, input, button {
-    font: 14px 'Roboto', sans-serif;
-  }
-
-  a {
-    text-decoration: none;
-    -webkit-text-decoration: none;
-  }
-
-  ul {
-    list-style: none;
-  }
-
-  button {
-    cursor: pointer;
-    background: none;
-    border: none;
-  }
-`;
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <GlobalStyle />
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
-    </>
+      <Routes />
+    </BrowserRouter>
   );
 }
 
---
+++ src/styles/global.js
@@ -0,0 +1,51 @@
+import { createGlobalStyle } from 'styled-components';
+import Reset from 'styled-reset';
+
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap');
+
+  ${Reset}
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: none;
+    box-sizing: border-box;
+  }
+
+  *:focus {
+    outline: 0;
+  }
+
+  html, body, #root {
+    margin: 0;
+    height: 100%;
+  }
+
+  body {
+    -webkit-font-smoothing: antialised;
+    /* overflow: hidden; */
+    /* height: 100%; */
+  }
+
+  body, input, button {
+    font: 14px 'Roboto', sans-serif;
+  }
+  input {
+    -webkit-appearance: none;
+  }
+  a {
+    text-decoration: none;
+    -webkit-text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  button {
+    cursor: pointer;
+    background: none;
+    border: none;
+  }
+`;
